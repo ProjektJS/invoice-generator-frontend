0c0f71590decf93cf1c04656df2cd49b32f98c1d
Łukasz Michalak <lukasz57570@gmail.com>
2020-08-07 18:05:52 +0200

    Fixed files names

---
+++ src/templates/index.js
@@ -0,0 +1,5 @@
+export { default as UserPageTemplate } from './UserPageTemplate';
+export { default as LandingPageTemplate } from './LandingPageTemplate';
+export { default as ErrorPageTemplate } from './ErrorPageTemplate';
+export { default as CreateInvoicePageTemplate } from './CreateInvoicePageTemplate';
+export { default as StepTemplate } from './StepTemplate';
